85f2a160dbd0cb14973f712779420c2c3c78733a
RatiRanjanDas <46194290+RatiRanjanDas@users.noreply.github.com>
2021-11-12 19:25:40 +0530

    noting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Jay Jagannath Swami Nayana Pathagami Bhava Tume!!</h1> \n');
+  res.send('<h1 style="color:green;">Jay Jagannath Swami Nayana Pathagami Bhava Tume!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
